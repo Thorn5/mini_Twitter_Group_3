359206f1185adae7fb5fd4f1d4bb9b1d57ca36eb
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2022-12-03 10:53:17 +0100

    Add files via upload

---
+++ JSON Files/messages.js
@@ -0,0 +1,92 @@
+export const messages = [
+    {
+        "_id": "134657911",
+        "text": "A house cat’s genome is 95.6 percent tiger, and they share many behaviors with their jungle ancestors, says Layla Morgan Wilde, a cat behavior expert and the founder of Cat Wisdom 101. These behaviors include scent marking by scratching, prey play, prey stalking, pouncing, chinning, and urine marking.",
+        "owner": 1
+    },
+    {
+        "_id": "245681512",
+        "text": "Cats are believed to be the only mammals who don’t taste sweetness.",
+        "owner": 1
+    },
+    {
+        "_id": "458235013",
+        "text": "Cats’ claws all curve downward, which means that they can’t climb down trees head-first. Instead, they have to back down the trunk.",
+        "owner": 1
+    },
+    {
+        "_id": "456478121",
+        "text": "my favorite animals at the zoo are just the random birds walking around like they belong. Go home pigeon, this is fancy bird town",
+        "owner": 2
+    },
+    {
+        "_id": "564726922",
+        "text": "I wonder if birds ever get sad and then are like, jk, I can fly.",
+        "owner": 2
+    },
+    {
+        "_id": "987556623",
+        "text": " Just like put a surfboard mouth on a big pigeon and make it like a loud idiot. I don't know",
+        "owner": 2
+    },
+    {
+        "_id": "789539531",
+        "text": "A lot of birds migrate which means, depending on the season, they fly elsewhere. This is often to do with the weather of different countries or places. If their food is running low, they need to find it somewhere else to live! Around 4,000 birds migrate.",
+        "owner": 3
+    },
+    {
+        "_id": "7861666832",
+        "text": "All birds have feathers and they are very useful for lots of different reasons. Feathers help birds to fly. They help to control the wind when flying through the air. They also keep birds warm in the winter! Did you know that birds also use their feathers to show off? A peacock does exactly this! A peacock will show all of his feathers to attract a mate.",
+        "owner": 3
+    },
+    {
+        "_id": "121588733",
+        "text": "Birds don’t have teeth which means they have to swallow food whole. They have an organ called a gizzard which grinds up their food. This then helps them to digest it properly.",
+        "owner": 3
+    },
+    {
+        "_id": "778842241",
+        "text": "Mice have big appetites. Despite their tiny bodies (and even smaller stomachs!), mice eat between 15 and 20 times a day. Because of their frequent eating habits, they prefer to build their homes near food sources.",
+        "owner": 4
+    },
+    {
+        "_id": "7896526942",
+        "text": "Mice can squeeze through openings as small as the size of a dime. This means that a small crack or opening on the exterior of your home (such as where utility pipes enter) is like an open door for mice. Prevent mice from gaining access to your home by sealing any openings on the exterior with a silicone caulk. You can also fill gaps and holes inside your home with steel wool.",
+        "owner": 4
+    },
+    {
+        "_id": "4861385743",
+        "text": "In the wild, mice usually only live for about five months, mostly because of predators such as cats, snakes and foxes. In a lab setting, mice can live for up to two years.",
+        "owner": 4
+    },
+    {
+        "_id": "789625351",
+        "text": "The most often quoted estimate of the number of fish species is 20,000. There may be as many as 20,000 more.",
+        "owner": 5
+    },
+    {
+        "_id": "891686852",
+        "text": "The largest fish in the world ... is actually a shark. Specifically, the whale shark, which grows to more than 50 feet long and may weigh several tons. The second largest is the basking shark, which can measure 35 to 40 feet long.",
+        "owner": 5
+    },
+    {
+        "_id": "456416153",
+        "text": "The smallest fish is the tiny goby, which lives in the Philippines. It rarely grows longer than half an inch at adulthood, yet is so abundant it supports a fishery.",
+        "owner": 5
+    },
+     {
+        "_id": "789496261",
+        "text": "being a best friend. is pretty easy. you just have to let them hug you. whenever they need to",
+        "owner": 6
+    },
+    {
+        "_id": "456896162",
+        "text": "be so good. and hope my politeness is eventually rewarded. with food",
+        "owner": 6
+    },
+    {
+        "_id": "214558863",
+        "text": "be a menace. and take every opportunity i have. to steal the food",
+        "owner": 6
+    }
+]
